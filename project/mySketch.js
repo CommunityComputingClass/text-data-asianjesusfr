f861ad4675be9cc696db253652149347529ab255
asianjesusfr <157668714+asianjesusfr@users.noreply.github.com>
2025-01-28 23:26:20 +0000

    submitting text 1

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,8 +1,23 @@
+let myFont;
+
+function preload(){
+  myFont = loadFont("go3v2.ttf")
+}
+
 function setup() {
   createCanvas(400, 400);
 }
-
 function draw() {
-  background(200);
-  
+  background(50);
+  strokeWeight(2);
+  fill(128 + sin(frameCount*0.1) * 128);
+  if (mouseIsPressed) {
+    stroke(255);
+  }
+  else {
+    noStroke();
+  }
+  textSize(12 + (mouseX / width)*72);
+  textFont(myFont);
+  text("Bought a property, I did", 50, 200);
 }
