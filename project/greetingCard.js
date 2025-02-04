605c47da6535aff3439a0deec25d7150aa798e02
asianjesusfr <157668714+asianjesusfr@users.noreply.github.com>
2025-02-04 20:06:25 +0000

    unfinshed array code

---
+++ project/greetingCard.js
@@ -0,0 +1,23 @@
+let peri = ["P- ","P- ","Periwinkle Purple"]
+let i = 0
+let xPos = 100
+let yPos = 250
+function setup() {
+    createCanvas(400, 400);
+  }
+
+  function draw() {
+    background(204, 204, 255);
+    noStroke();
+    fill(0);
+    text("Whats your favorite color?", 50, 200);
+    text(peri[i], xPos, yPos)
+   
+   
+
+        
+        }
+
+function mousePressed {
+    i++
+}
